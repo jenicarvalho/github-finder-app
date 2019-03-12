35118ffbfe54cbce05aacaeb3636900c380262d2
Jeniffer Carvalho <jeniffer@agenciapixels.com>
2019-03-11 23:18:08 -0300

    criado métodos clearProfile() showAlert() clearAlert() e showRepos()

--- ui.js
+++ ui.js
@@ -3,6 +3,7 @@ class UI {
     this.profile = document.getElementById('profile');
   }
 
+  //display profile
   showProfile(user) {
     this.profile.innerHTML = `
       <div class="card card-body mb-3">
@@ -31,4 +32,68 @@ class UI {
       <div id="repos"></div>
     `;
   }
+
+  //show repos
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach(repo => {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+              <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+              <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+              <span class="badge badge-success">Forks: ${repo.forms_count}</span>  
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+    //output
+    console.log(output);
+
+    document.getElementById('repos').innerHTML = output;
+  }
+
+  //Clear profile
+  clearProfile() {
+    this.profile.innerHTML = '';
+  }
+
+  //show alert message
+  showAlert(message, className) {
+    //clear any remaining alerts
+    this.clearAlert();
+    // create div
+    const div = document.createElement('div');
+    // ad class
+    div.className = className;
+    //add text
+    div.appendChild(document.createTextNode(message));
+    //get parent
+    const container = document.querySelector('.searchContainer');
+    //get searchbox
+    const search = document.querySelector('.search');
+    //insert alert
+    container.insertBefore(div, search);
+
+    //timeout after 3 sec
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  //clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if(currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
 }
